3209af50934721dca9aac041b192fbdf3f0312f2
shaksham@squapl.com <shaksham@squapl.com>
2026-04-28 00:39:52 +0530

    getPower

---
+++ Maths/Power.java
@@ -0,0 +1,21 @@
+class Power{
+    static int getPower(int x, int n)
+    {
+     int res =1;
+     while (n>0)
+     {
+        if(n%2!=0)
+        {
+            res= res*x;
+        }
+        x=x*x;
+        n=n/2;
+     }
+     return res;
+    }
+    public static void main(String[] args)
+    {
+       int result = getPower(3, 4);
+       System.out.println(result);
+    }
+}
